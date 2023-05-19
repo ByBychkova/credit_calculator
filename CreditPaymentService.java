474ef1381b6ac2364c0ad0fd5f8e59b147c1e5ad
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-05-19 17:08:06 +0300

    Credit

---
+++ CreditPaymentService.java
@@ -0,0 +1,13 @@
+public class CreditPaymentService {
+
+    public int calculate(int sc, int np, double st) {
+
+        int result;
+
+        Math.pow(a1, b2);
+        int chek = sc * (st / 1 - Math.pow(a1, b2)) = (sc * st) / 1 - (1 / Math.pow(a2, b3)) = sc(st + (st / (Math.pow(a1, b2) - 1);
+        result = (int) chek;
+        return result;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CreditPaymentServic service = new CreditPaymentServic();
+
+        int sc; //первонач сумма кредита
+        int np = 12
+        int a1 = 1 + st;
+        int b2 = -12;
+        double st = 0.0_999;
+
+        int a2 = (1+st);
+        int b3 = 12;
+
+        int chek;
+        chek = service.caiculate (sc,np,st);
+        System.out.println(chek);
     }
 }
