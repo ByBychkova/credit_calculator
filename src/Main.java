17f047560bd70c3bdf4459c7eb1ca7632dd49ea8
Bychkova Mariya <bychk-mariya@yandex.ru>
2023-05-25 16:01:55 +0300

    New scenari

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        CreditPaymentServic service = new CreditPaymentServic();
+        W service = new W();
+        double i = 0.099_9 / 12 / 100;
 
-        int sc; //первонач сумма кредита
-        int np = 12
-        int a1 = 1 + st;
-        int b2 = -12;
-        double st = 0.0_999;
+        double a1 = 1 + i;
+        int n1 = 12;
+        System.out.printf("Число a1 в 12 степени равно %.0f \n", Math.pow(a1, n1));
+        // коэффициент аннуитета;
+        double k = (i * (Math.pow(a1, n1))) / ((Math.pow(a1, n1)) - 1);
 
-        int a2 = (1+st);
-        int b3 = 12;
+        int s = 1_000_000;
 
-        int chek;
-        chek = service.caiculate (sc,np,st);
-        System.out.println(chek);
+        System.out.println(k);
+
+        double a = service.calculate(0.08337843396539649, 1_000_000);
+        System.out.println(a);
     }
 }
---
+++ src/W.java
@@ -0,0 +1,11 @@
+public class W {
+
+    public int calculate(double k,int s) {
+
+        int result;
+        double a = k*s;
+        result = (int) a;
+        return result;
+
+    }
+}
